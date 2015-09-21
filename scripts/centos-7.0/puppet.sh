2226da562161e72a736db82e24ea4315959e67af
Dan <dan@ifunky.net>
2015-09-21 16:37:02 +0100

    Adding puppet to global profiles

--- scripts/centos-7.0/puppet.sh
+++ scripts/centos-7.0/puppet.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install puppet-agent
-PATH=/opt/puppetlabs/bin:$PATH;export PATH
+
+# Add puppet bin path to global profile for all users
+echo 'pathmunge /opt/puppetlabs/bin' > /etc/profile.d/puppet.sh
+chmod +x /etc/profile.d/puppet.sh
+#PATH=/opt/puppetlabs/bin:$PATH;export PATH
