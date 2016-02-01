68e6e11f4fc499bd6152f3bf4d4708a6e9e11a10
Dan <dan@ifunky.net>
2016-02-01 12:09:39 +0000

    Added no_proxy env to base bash file

--- scripts/centos-7.0/base.sh
+++ scripts/centos-7.0/base.sh
@@ -10,3 +10,7 @@ if [ ! -z "$http_proxy" ]; then
     echo "export http_proxy=$http_proxy" >> /etc/profile.d/proxy.sh
     echo "export https_proxy=$http_proxy" >> /etc/profile.d/proxy.sh
 fi
+
+if [ ! -z "$no_proxy" ]; then
+    echo "export no_proxy=$no_proxy" >> /etc/profile.d/proxy.sh
+fi
