b7dd6ec66d3d0e0ee2b0ff4ebc67f206299efacb
Dan <dan@ifunky.net>
2017-10-17 08:10:13 +0100

    Remove debug statements and reboot for win2016 standard build

--- scripts/windows/vm-guest-tools.ps1
+++ scripts/windows/vm-guest-tools.ps1
@@ -1,17 +1,9 @@
-mkdir c:\temp
-dir f:
-
-$from = "f:\*"
-$to = "c:\temp"
-Copy-Item $from $to -recurse
-
-
 Write-Host 'Installing certs'
-$files = Get-ChildItem c:\temp\cert\*.cer
-foreach ($file in $files) { & c:\temp\cert\VBoxCertUtil.exe add-trusted-publisher $file --root $file }
+$files = Get-ChildItem f:\cert\*.cer
+foreach ($file in $files) { & f:\cert\VBoxCertUtil.exe add-trusted-publisher $file --root $file }
 
 Write-Host 'Installing Guest Additions'
-& c:\temp\VBoxWindowsAdditions.exe /S /I
+& f:\VBoxWindowsAdditions.exe /S /I
 
 Write-Host "Sleeping for 60 seconds so we are sure the tools are installed before reboot"
 Start-Sleep -s 60
